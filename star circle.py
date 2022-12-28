76319baee0844214b61996381b1369327ed0089a
E.Christo Raymonde <101086026+ec7719@users.noreply.github.com>
2022-12-28 21:35:27 +0530

    Add files via upload

---
+++ star circle.py
@@ -0,0 +1,28 @@
+# import package
+import turtle 
+
+t1=turtle.Turtle()
+t1.pensize(3)
+t1.fillcolor("pink")
+# for default shape
+
+
+n=140
+m=200
+for i in range(90):
+  t1.circle(90)
+  t1.forward(m)
+  t1.pencolor("blue")
+  t1.right(n)
+  t1.circle(90)
+  t1.pencolor("black")
+  t1.forward(m)
+  t1.pencolor("blue")
+  t1.right(n)
+  t1.pencolor("blue")
+
+ 
+
+
+ 
+ 
